0d0cab568842163a3c841e916f69212552b3ac0a
thdafamily <thiagovieirasilva35@gmail.com>
2026-06-11 19:53:43 -0300

    Esqueleto do código

---
+++ trabMonitoramento.py
@@ -0,0 +1,64 @@
+alunos = ['Thiago', 'Guilherme', 'Fábio']
+meses = ['Mês 1', 'Mês 2', 'Mês 3', 'Mê 4']
+
+
+matriz_gordura = [
+    [0.0, 0.0, 0.0, 0.0], 
+    [0.0, 0.0, 0.0, 0.0], 
+    [0.0, 0.0, 0.0, 0.0]  
+]
+
+matriz_treinos = [
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0], 
+    [0, 0, 0, 0] 
+]
+print("========================================")
+print("  SISTEMA DE MONITORAMENTO VIP - LPI  ")
+print("========================================")
+print("Integrantes: Thiago, Guilherme, Fábio")
+print("----------------------------------------\n")
+while True:
+    print("\n--- MENU DE OPÇÕES ---")
+    print("1. Cadastrar Avaliações e Frequência")
+    print("2. Listar Relatório de Evolução")
+    print("3. Atualizar Dados de Monitoramento")
+    print("4. Pesquisa com Filtro")
+    print("5. Sair do Programa")
+    
+    opcao = input("Escolha uma opção: ")
+
+    if opcao == '1':
+        print("\n>> Opção 1: Módulo de Cadastro...")
+        
+
+
+        pass 
+
+    elif opcao == '2':
+        print("\n>> Opção 2: Gerando Relatório...")
+        
+
+
+        pass
+
+    elif opcao == '3':
+        print("\n>> Opção 3: Atualização de Dados...")
+        
+
+
+        pass
+
+    elif opcao == '4':
+        print("\n>> Opção 4: Pesquisa Gerencial...")
+        
+
+
+        pass
+
+    elif opcao == '5':
+        print("\nEncerrando o sistema...")
+        break
+
+    else:
+        print("\n[ERRO] Opção inválida! Digite um número de 1 a 5.")
